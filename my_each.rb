83f0717d4b2ddb53a69745dee318caf446fe0417
TJZii <tjhaweszii@gmail.com>
2020-11-21 02:12:46 -0800

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(numbers) # put argument(s) here
   # code here
+  z = 0
+  while(z < numbers.length)
+    yield(numbers[z])
+    z += 1
+  end
+  numbers
 end
